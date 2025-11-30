6e37517f9df398996a0140afc6a67feba1be81b0
henintsoapaul <henintsoapaul@gmail.com>
2025-11-30 17:18:44 +0300

    fix: exception msg

--- src/main/java/com/tsoa/digibank/exceptions/BankAccountNotFoundException.java
+++ src/main/java/com/tsoa/digibank/exceptions/BankAccountNotFoundException.java
@@ -1,7 +1,7 @@
 package com.tsoa.digibank.exceptions;
 
 public class BankAccountNotFoundException extends Exception{
-    public BankAccountNotFoundException(String message) {
-        super(message);
+    public BankAccountNotFoundException() {
+        super("BankAccount not found");
     }
 }
