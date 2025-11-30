8e80952ca354940f47d5aafd073b691a1b742beb
henintsoapaul <henintsoapaul@gmail.com>
2025-11-30 18:31:47 +0300

    feat: add Dto

---
+++ src/main/java/com/tsoa/digibank/data/dtos/operation/TransferRequestDTO.java
@@ -0,0 +1,11 @@
+package com.tsoa.digibank.data.dtos.operation;
+
+import lombok.Data;
+
+@Data
+public class TransferRequestDTO {
+    private  String accountSource;
+    private  String accountDestination;
+    private double amount;
+    private String description;
+}
