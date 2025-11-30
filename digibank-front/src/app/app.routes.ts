5936f7a4a8fdbbda28f1f1e8afef5abd8cd72d01
henintsoapaul <henintsoapaul@gmail.com>
2025-11-30 22:04:19 +0300

    feat: set navbar

--- digibank-front/src/app/app.routes.ts
+++ digibank-front/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {Accounts} from './accounts/accounts';
+import {Customers} from './customers/customers';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'accounts',
+    component: Accounts
+  },
+  {
+    path: 'customers',
+    component: Customers
+  },
+];
