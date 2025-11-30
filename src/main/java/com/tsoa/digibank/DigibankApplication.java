d8a90bf1f553a24a36b756a0cf9abf651f181d1b
henintsoapaul <henintsoapaul@gmail.com>
2025-11-30 18:54:23 +0300

    feat: db seed

--- src/main/java/com/tsoa/digibank/DigibankApplication.java
+++ src/main/java/com/tsoa/digibank/DigibankApplication.java
@@ -1,7 +1,20 @@
 package com.tsoa.digibank;
 
+import com.tsoa.digibank.data.dtos.*;
+import com.tsoa.digibank.data.enums.*;
+import com.tsoa.digibank.data.models.*;
+import com.tsoa.digibank.data.models.bankaccount.*;
+import com.tsoa.digibank.repositories.*;
+import com.tsoa.digibank.services.bankaccount.*;
+import com.tsoa.digibank.services.customer.*;
+import com.tsoa.digibank.services.operation.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class DigibankApplication {
@@ -10,4 +23,58 @@ public class DigibankApplication {
         SpringApplication.run(DigibankApplication.class, args);
     }
 
+    private static Customer mockCustomer(String name) {
+        Customer customer = new Customer();
+        customer.setName(name);
+        customer.setEmail(name + "@gmail.com");
+        return customer;
+    }
+
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository,
+                            AccountOperationRepository accountOperationRepository,
+                            BankAccountRepository bankAccountRepository) {
+        return args -> {
+            Stream.of("Amina", "Souad", "Mohammed").forEach(
+                    name -> {
+                        Customer customer = mockCustomer(name);
+                        customerRepository.save(customer);
+                    }
+            );
+
+            double rand = Math.random();
+
+            customerRepository.findAll().forEach(customer -> {
+                CurrentAccount currentAccount = new CurrentAccount();
+                currentAccount.setId(UUID.randomUUID().toString());
+                currentAccount.setBalance(rand * 90000);
+                currentAccount.setCreateAt(new java.util.Date());
+                currentAccount.setStatus(AccountStatus.CREATED);
+                currentAccount.setCustomer(customer);
+                currentAccount.setOverDraft(9000);
+                bankAccountRepository.save(currentAccount);
+
+                SavingAccount savingAccount = new SavingAccount();
+                savingAccount.setId(UUID.randomUUID().toString());
+                savingAccount.setBalance(rand * 90000);
+                savingAccount.setCreateAt(new java.util.Date());
+                savingAccount.setStatus(AccountStatus.CREATED);
+                savingAccount.setCustomer(customer);
+                savingAccount.setInterestRate(10);
+                bankAccountRepository.save(savingAccount);
+            });
+
+            bankAccountRepository.findAll().forEach(bankAccount -> {
+                for (int i = 0; i < 5; i++) {
+                    AccountOperation accountOperation = new AccountOperation();
+                    accountOperation.setOperationDate(new java.util.Date());
+                    accountOperation.setAmount(rand * 12000);
+                    accountOperation.setType(rand > 0.5 ? OperationType.DEBIT : OperationType.CREDIT);
+                    accountOperation.setBankAccount(bankAccount);
+                    accountOperationRepository.save(accountOperation);
+                }
+            });
+
+        };
+    }
 }
