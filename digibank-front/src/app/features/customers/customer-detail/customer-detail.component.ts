4201301eaac5ae21ad233bbbb0d0e4903f9def35
henintsoapaul <henintsoapaul@gmail.com>
2025-12-01 19:15:37 +0300

    feat(front): naive customer details

--- digibank-front/src/app/features/customers/customer-detail/customer-detail.component.ts
+++ digibank-front/src/app/features/customers/customer-detail/customer-detail.component.ts
@@ -1,25 +1,43 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {Customer} from '../models/customer.model';
 import {ActivatedRoute} from '@angular/router';
+import {BankAccountService} from '../../bank-accounts/services/bank-account.service';
+import {BankAccount} from "../../bank-accounts/models/bank-account.model";
+import {Skeleton} from 'primeng/skeleton';
 
 @Component({
   selector: 'app-customer-detail',
-  imports: [],
+  imports: [
+    Skeleton,
+  ],
   templateUrl: './customer-detail.component.html',
 })
-export class CustomerDetailComponent {
-  customerId: string = '';
+export class CustomerDetailComponent implements OnInit {
   customer?: Customer = undefined;
 
+  accounts: BankAccount[] = [];
+  loadingAccounts: boolean = false;
+
   private route = inject(ActivatedRoute);
+  private accountService = inject(BankAccountService);
 
-  constructor() {
+  ngOnInit() {
     this.route.data.subscribe((data) => {
-      this.customer = data['customer'];
+      this.customer = data['customer'] as Customer;
+      this._fetchCustomerAccounts(this.customer.id);
     })
+  }
 
-    this.route.params.subscribe((params) => {
-      this.customerId = params['id'] as string;
-    })
+  private _fetchCustomerAccounts(customerId: number) {
+    this.accountService.getBankAccountsByCustomer(customerId).subscribe({
+      next: (accounts) => {
+        this.accounts = accounts;
+        this.loadingAccounts = false;
+      },
+      error: (err) => {
+        console.log(err);
+        this.loadingAccounts = false;
+      }
+    });
   }
 }
