4b2ad797e9850bad09d1ae0c7d207c040af61254
henintsoapaul <henintsoapaul@gmail.com>
2025-12-01 09:57:54 +0300

    feat: theme transition

--- digibank-front/src/app/core/layout.service.ts
+++ digibank-front/src/app/core/layout.service.ts
@@ -1,4 +1,5 @@
-import {Injectable, signal} from '@angular/core';
+import {effect, Injectable, signal, WritableSignal} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
 
 export interface AppState {
   preset: string;
@@ -18,14 +19,82 @@ export class LayoutService {
     darkMode: false,
   };
 
-  appState = signal<AppState>(this._appState);
+  appState: WritableSignal<AppState> = signal<AppState>(this._appState);
+
+  _initialized: boolean = false;
+
+  _transitionComplete: WritableSignal<boolean> = signal<boolean>(false);
+
+  _appStateUpdate: Subject<AppState> = new Subject<AppState>();
+
+  constructor() {
+    effect(() => {
+      const appState = this.appState();
+      if (appState) {
+        this.onAppStateUpdate();
+      }
+    });
+
+    effect(() => {
+      const state = this.appState();
+
+      if (!this._initialized || !state) {
+        this._initialized = true;
+        return;
+      }
+
+      this._handleDarkModeTransition(state);
+    });
+  }
 
   toggleDarkMode(appState?: AppState): void {
+    document.documentElement.classList.add('theme-transition');
+
     const _appState = appState || this.appState();
     if (_appState.darkMode) {
       document.documentElement.classList.add('p-dark');
     } else {
       document.documentElement.classList.remove('p-dark');
     }
+
+    // Remove transition class after animation
+    setTimeout(() => {
+      document.documentElement.classList.remove('theme-transition');
+    }, 300);
+  }
+
+  onAppStateUpdate() {
+    this._appState = {...this.appState()};
+    this._appStateUpdate.next(this.appState());
+    this.toggleDarkMode();
+  }
+
+  private _handleDarkModeTransition(config: AppState): void {
+    if ((document as any)._startViewTransition) {
+      this._startViewTransition(config);
+    } else {
+      this.toggleDarkMode(config);
+      this._onTransitionEnd();
+    }
+  }
+
+  private _startViewTransition(config: AppState): void {
+    const transition = (document as any).startViewTransition(() => {
+      this.toggleDarkMode(config);
+    });
+
+    transition.ready
+      .then(() => {
+        this._onTransitionEnd();
+      })
+      .catch(() => {
+      });
+  }
+
+  private _onTransitionEnd() {
+    this._transitionComplete.set(true);
+    setTimeout(() => {
+      this._transitionComplete.set(false);
+    });
   }
 }
