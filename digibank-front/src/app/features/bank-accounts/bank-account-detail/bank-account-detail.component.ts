84f672595fefcc487a0793fde55a352352041275
henintsoapaul <henintsoapaul@gmail.com>
2025-12-18 19:15:05 +0300

    feat(front): refactor - operation form dialog

--- digibank-front/src/app/features/bank-accounts/bank-account-detail/bank-account-detail.component.ts
+++ digibank-front/src/app/features/bank-accounts/bank-account-detail/bank-account-detail.component.ts
@@ -1,14 +1,13 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {BankAccount} from "../../bank-accounts/models/bank-account.model";
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 import {TableModule} from 'primeng/table';
-import {Operation, OperationReq, OperationType} from '../../operations/models/operation.model';
+import {Operation} from '../../operations/models/operation.model';
 import {OperationService} from '../../operations/services/operation.service';
 import {OperationList} from '../../operations/operation-list/operation-list';
 import {Button} from 'primeng/button';
-import {Dialog} from 'primeng/dialog';
-import {InputText} from 'primeng/inputtext';
+import {OperationForm} from '../../operations/operation-form/operation-form';
 
 @Component({
   selector: 'app-bank-account-detail',
@@ -17,8 +16,7 @@ import {InputText} from 'primeng/inputtext';
     TableModule,
     OperationList,
     Button,
-    Dialog,
-    InputText,
+    OperationForm,
   ],
   templateUrl: './bank-account-detail.component.html',
 })
@@ -31,25 +29,12 @@ export class BankAccountDetailComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private operationService = inject(OperationService);
 
-  private fb = inject(FormBuilder);
-  operationForm!: FormGroup;
-  operationTypeList!: OperationType[];
-
   ngOnInit() {
     // Fetch then set data
     this.route.data.subscribe((data) => {
       this.bankAccount = data['account'] as BankAccount;
       this._fetchCustomerAccounts(this.bankAccount.id);
     })
-
-    // Setup operation dialog form
-    this.operationForm = this.fb.group({
-      amount: ['500', [Validators.required]],
-      type: ['', [Validators.required]],
-      description: [''],
-    });
-
-    this.operationTypeList = Object.values(OperationType);
   }
 
   private _fetchCustomerAccounts(accountId: string) {
@@ -68,39 +53,13 @@ export class BankAccountDetailComponent implements OnInit {
   // -- DIALOG --
 
   dialogBtnText = "Add operation";
-  dialogTitle = "Add operation";
   dialogVisible = false;
 
-  toggleDialogVisibility() {
-    this.dialogVisible = !this.dialogVisible;
+  showDialog() {
+    this.dialogVisible = true;
   }
 
-  submitDialog() {
-    // Prepare request
-    const operationReq = new OperationReq(
-      this.getFieldValue("amount") as number,
-      this.getFieldValue("type") as OperationType,
-      this.getFieldValue("description"),
-    );
-    console.log(operationReq)
-
-    // Send to API
-    const response$ = this.operationService.create(operationReq);
-
-    // Show response in modal pop
-    // ...
-  }
-
-  getFieldValue(field: string): any {
-    return this.operationForm.get(field)!.value!;
-  }
-
-  isInvalid(field: string): boolean | undefined {
-    const isInvalid = this.operationForm.get(field)?.invalid
-      && (
-        this.operationForm.get(field)?.dirty
-        || this.operationForm.get(field)?.touched
-      );
-    return isInvalid;
+  onCloseDialog(_: boolean) {
+    this.dialogVisible = false;
   }
 }
